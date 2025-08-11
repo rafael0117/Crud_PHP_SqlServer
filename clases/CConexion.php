a85d91379f7e9585a9c50136a1d1092495072d77
rafael0117 <mamanirafael324@gmail.com>
2025-08-11 14:31:45 -0500

    Conexion database

--- clases/CConexion.php
+++ clases/CConexion.php
@@ -2,18 +2,18 @@
 Class CConexion 
 {
     public static function ConexionBD(){
-        $host = "";
-        $dbname = "";
-        $username = "";
-        $password = "";
-        $port = "";
+        $host = "127.0.0.1";
+        $dbname = "bdcole";
+        $username = "rootsqlserver";
+        $password = "rootsqlserver";
+        $port = "1433";
 
         try{
-            $conn = new PDO();
+            $conn = new PDO("sqlsrv:server=$host,$port;Database=$dbname",$username,$password);
             $conn -> setAttribute(PDO:: ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
             echo("Se conecto a la base de datos");
         }
-        catch(PDOException){
+        catch(PDOException $pe){
             die("No se logro conectar a la base de datos". $pe -> getMessage());
 
         }
